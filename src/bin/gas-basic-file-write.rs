d62a661224d37f160ac2579165b8a948e7bc1db6
roc <peng.yin@geneus-tech.com>
2025-05-30 14:19:15 +0000

    dio fast

--- src/bin/gas-basic-file-write.rs
+++ src/bin/gas-basic-file-write.rs
@@ -48,7 +48,7 @@ fn aligned_alloc(size: usize) -> Vec<u8> {
     }
 }
 
-fn vanilla_file_write_dio(cli: &Cli) {
+fn file_write_dio(cli: &Cli) {
     let data_size = 1024 * 1024 * 1024 * 10; // 2 GB
     let data = aligned_alloc(data_size);
     // Open a file in write mode, creating it if it doesn't exist
@@ -72,14 +72,42 @@ fn vanilla_file_write_dio(cli: &Cli) {
     let elapsed = instant.elapsed().as_secs() as usize;
     let bytes_per_sec = (data_size / elapsed) as f64;
     let mb_per_sec = bytes_per_sec / (1024.0 * 1024.0);
-    println!("MB per second: {:.2}MB/s", mb_per_sec); // 568MB/s . 4M block 640MB/s
+    println!("MB per second: {:.2}MB/s", mb_per_sec); // 4M block 2.5GB/s
+}
+
+fn file_write_rio(cli: &Cli) {
+    let data_size = 1024 * 1024 * 1024 * 10; // 2 GB
+    let data = aligned_alloc(data_size);
+    // Open a file in write mode, creating it if it doesn't exist
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .custom_flags(libc::O_DIRECT) // Use O_DIRECT for direct I/O
+        .open(&cli.out_path)
+        .expect("Unable to create file");
+
+    let mut start = 0;
+    let buf_size = 4 * 1024 * 1024; // 1 MB buffer size
+    let instant = Instant::now();
+    while start < data.len() {
+        // Write data to the file
+        let end = std::cmp::min(start + buf_size, data.len());
+        file.write_all(&data[start..end])
+            .expect("Unable to write data");
+        start = end;
+    }
+    let elapsed = instant.elapsed().as_secs() as usize;
+    let bytes_per_sec = (data_size / elapsed) as f64;
+    let mb_per_sec = bytes_per_sec / (1024.0 * 1024.0);
+    println!("MB per second: {:.2}MB/s", mb_per_sec); // 4M block 2.5GB/s
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.mode.as_str() {
         "vanilla" => vanilla_file_write(&cli),
-        "vanilla-dio" => vanilla_file_write_dio(&cli),
+        "dio" => file_write_dio(&cli),
+        "rio" => file_write_rio(&cli),
         _ => panic!("Unknown mode: {}", cli.mode),
     }
 }
