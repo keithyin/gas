a48c304d756ae0675f7f78a38313312b744bf6c6
keithyin <yinpenghhz@hotmail.com>
2025-05-31 20:10:19 +0800

    update

--- src/bin/gas-basic-file-write.rs
+++ src/bin/gas-basic-file-write.rs
@@ -4,7 +4,6 @@ use std::{
     cell::RefCell,
     fs::OpenOptions,
     io::Write,
-    ops::DerefMut,
     os::{fd::AsRawFd, unix::fs::OpenOptionsExt},
     time::Instant,
 };
@@ -97,10 +96,6 @@ impl AlignedBuffer {
         }
     }
 
-    fn as_slice(&self) -> &[u8] {
-        &self.buffer[0..self.data_size]
-    }
-
     fn fill_buffer(&mut self, data: &[u8]) -> usize {
         let remaining = self.buffer.len() - self.data_size;
         let to_cpy = remaining.min(data.len());
@@ -112,11 +107,9 @@ impl AlignedBuffer {
         self.data_size = 0;
         self
     }
-}
 
-impl AsRef<[u8]> for AlignedBuffer {
-    fn as_ref(&self) -> &[u8] {
-        self.as_slice()
+    fn as_mut_ptr(&mut self) -> *mut u8 {
+        (self.buffer[..self.data_size]).as_mut_ptr()
     }
 }
 
@@ -198,9 +191,73 @@ fn file_write_uring1(cli: &Cli) {
                 .fill_buffer(&data[start..end]);
             let write_event = opcode::Write::new(
                 types::Fd(file.as_raw_fd()),
-                rio_buffers[valid_idx].borrow_mut().buffer.as_mut_ptr(),
+                rio_buffers[valid_idx].borrow_mut().as_mut_ptr(),
                 (end - start) as u32,
             )
+            .offset(start as u64)
+            .build()
+            .user_data(valid_idx as u64);
+
+            unsafe {
+                ring.submission()
+                    .push(&write_event)
+                    .expect("Failed to push write event");
+            }
+            start = end;
+        } else {
+            ring.submit_and_wait(1).unwrap();
+            let cqe = ring.completion().next().expect("No completion event");
+            valid_idx_queue.push(cqe.user_data() as usize);
+        }
+    }
+    ring.submit_and_wait(io_depth).unwrap();
+    drop(file);
+
+    let elapsed = instant.elapsed().as_secs() as usize;
+    let bytes_per_sec = (data_size / elapsed) as f64;
+    let mb_per_sec = bytes_per_sec / (1024.0 * 1024.0);
+    println!("MB per second: {:.2}MB/s", mb_per_sec); // 4M block 2.5GB/s
+}
+
+fn file_write_uring2(cli: &Cli) {
+    let data_size = 1024 * 1024 * 1024 * 10; // 2 GB
+    let mut data = aligned_alloc(data_size);
+    data.iter_mut().for_each(|v| *v = 'A' as u8);
+
+    // Open a file in write mode, creating it if it doesn't exist
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .custom_flags(libc::O_DIRECT) // Use O_DIRECT for direct I/O
+        .open(&cli.out_path)
+        .expect("Unable to create file");
+
+    let mut start = 0;
+    let buf_size = 4 * 1024 * 1024; // 1 MB buffer size
+    let instant = Instant::now();
+
+    let mut valid_idx_queue = FixedSizeStack::new(8);
+    valid_idx_queue.fill_stack(&vec![7, 6, 5, 4, 3, 2, 1, 0]);
+
+    let io_depth = 8;
+    let rio_buffers = vec![RefCell::new(AlignedBuffer::new(buf_size)); io_depth];
+    let mut ring = IoUring::new(io_depth as u32).expect("Failed to create IoUring");
+
+    // init completions
+    while start < data.len() {
+        // Write data to the file
+        let end = std::cmp::min(start + buf_size, data.len());
+        if let Some(valid_idx) = valid_idx_queue.pop() {
+            rio_buffers[valid_idx]
+                .borrow_mut()
+                .clear_buf()
+                .fill_buffer(&data[start..end]);
+            let write_event = opcode::Write::new(
+                types::Fd(file.as_raw_fd()),
+                rio_buffers[valid_idx].borrow_mut().as_mut_ptr(),
+                (end - start) as u32,
+            )
+            .offset(start as u64)
             .build()
             .user_data(valid_idx as u64);
 
@@ -231,6 +288,7 @@ fn main() {
         "vanilla" => vanilla_file_write(&cli),
         "dio" => file_write_dio(&cli),
         "uring1" => file_write_uring1(&cli),
+        "uring2" => file_write_uring1(&cli),
         _ => panic!("Unknown mode: {}", cli.mode),
     }
 }
